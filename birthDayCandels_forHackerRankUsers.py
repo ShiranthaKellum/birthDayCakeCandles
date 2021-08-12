1e9a38962ada386cea18d070a3f746828fa5dc6d
ShiranthaKellum <73046831+ShiranthaKellum@users.noreply.github.com>
2021-08-12 13:15:27 +0530

    Update birthDayCandels_forHackerRankUsers.py

--- birthDayCandels_forHackerRankUsers.py
+++ birthDayCandels_forHackerRankUsers.py
@@ -2,7 +2,7 @@
 n = int (input ())
 
 candels = list (map (int, input ().strip ().split ()))          # convert string squence with space in to an integer list
-                                                                # without strip () method, it doesn't matter 
+                                                                # without strip () method, it doesn't matter. It just removes white spaces
 candels.sort ()                                                 # sort list
 
 print (candels.count (candels [n-1]))                           
